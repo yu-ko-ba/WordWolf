48b01485a146f52019db24532f2f59904521e340
yu-ko-ba <lily_lime_mint@outlook.com>
2020-06-15 16:39:08 +0900

    WordWolfクラス(たぶん)完成

--- WordWolf.java
+++ WordWolf.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 
 class WordWolf {
-  String[] players;
-  String[] words;
+  final String[] players;
+  final String[] words;
 
-  int wolfNumber;
+  private final int wolfNumber;
 
-  String wolfWord;
-  String citizenWord;
+  private final String wolfWord;
+  private final String citizenWord;
 
 
   public WordWolf(String[] players, String[] words) {
@@ -16,13 +16,37 @@ class WordWolf {
 
     wolfNumber = new Random().nextInt(players.length);
 
+    // ウルフのワードを決める
     wolfWord = words[new Random().nextInt(words.length)];
+
+    // 市民のワードを決める
     while (true) {
       int citizenWordNumber = new Random().nextInt(words.length);
+
+      // ウルフと異なるワードが選ばれたら採用する
       if (!wolfWord.equals(words[citizenWordNumber])) {
         citizenWord = words[citizenWordNumber];
         break;
       }
     }
+  }
+
+  // nameがウルフかを返す
+  boolean WolfIs(String name) {
+      if (name.equals(players[wolfNumber])) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+
+  // nameのお題を返す
+  String MessageFor(String name) {
+      String word = citizenWord;
+      if (WolfIs(name)) {
+          word = wolfWord;
+      }
+
+      return "お題は「" + word + "」です";
   }
 }
